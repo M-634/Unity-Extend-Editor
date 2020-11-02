a194fa9f075f4e8901638508fa7ebe90415b6b41
teruteru634 <miyanagateru634@gmail.com>
2020-11-02 22:14:04 +0900

    チーム制作でデザイナーさんがフォルダー構成を統一するために、必要なフォルダーを生成する拡張機能を追加した

---
+++ Assets/Editor/CreateGraphicsFolders.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;//拡張エディターを作成するのに必要
+#endif
+
+/// <summary>
+/// デザイナーさん用のフォルダーを作成するクラス
+/// </summary>
+public class CreateGraphicsFolders
+{
+    static int m_customFolerNumber = 0;
+
+    [MenuItem("M-634/Create/GraphicsFolders")]
+    public static void Create()
+    {
+        string graphicsfolderPath = Path.Combine(Application.dataPath, "Graphics");
+
+        //Graphicsフォルダーを作成する
+        if (!Directory.Exists(graphicsfolderPath))
+        {
+            //初めて作成した
+            Directory.CreateDirectory(graphicsfolderPath);
+            m_customFolerNumber = 0;
+            //Debug.Log("Initialize GraphiscsFolders....");
+        }
+
+        string customFolderPath;
+        if (m_customFolerNumber == 0)
+        {
+            customFolderPath = Path.Combine(graphicsfolderPath, "Custom");
+        }
+        else
+        {
+            customFolderPath = Path.Combine(graphicsfolderPath, "Custom" + m_customFolerNumber.ToString());
+        }
+        m_customFolerNumber++;
+        Directory.CreateDirectory(customFolderPath);
+
+        string[] pathes = { Path.Combine(customFolderPath, "Textures"),Path.Combine(customFolderPath, "Materials"),
+            Path.Combine(customFolderPath, "Meshes"), Path.Combine(customFolderPath, "Prefabs") };
+
+        foreach (var path in pathes)
+        {
+            Directory.CreateDirectory(path);
+        }
+        //Debug.Log("実行されてるよ");
+    }
+}
