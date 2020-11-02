c6c35a3a2ec2d8de0e883cba715d4d32c27b1fac
teruteru634 <miyanagateru634@gmail.com>
2020-11-02 22:50:32 +0900

    Unity からPhotshopを開くエディター拡張

---
+++ Assets/Editor/OpenPhotoshop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Diagnostics;
+using System;
+
+public class OpenPhotoshop : MonoBehaviour
+{
+    const string ItemName = "Assets/Open in Photoshop";
+
+    [MenuItem(ItemName,false)]
+    static void Opne()
+    {
+        foreach (var guid in Selection.assetGUIDs)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var fullPath = Path.GetFullPath(path);
+            Process.Start("photoshop", fullPath);
+        }
+    }
+}
