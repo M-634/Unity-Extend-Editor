28e42fa2908b8e2883300cdbd8a2cbeee73080cd
teruteru634 <miyanagateru634@gmail.com>
2020-11-04 11:18:39 +0900

    ファイルの名前をコピーするやつ、こっちにも入れとく

---
+++ Assets/Editor/CopyFileName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+
+public class CopyFileName : MonoBehaviour
+{
+    [MenuItem("Assets/Copy FileName", false)]
+    static void Execute()
+    {
+        //ファイルの名前を取得
+        int instanceID = Selection.activeInstanceID;
+        string path = AssetDatabase.GetAssetPath(instanceID);
+        string fileName = Path.GetFileName(path);
+        fileName = fileName.Split('.').FirstOrDefault();
+
+        //クリップボードにコピーする
+        GUIUtility.systemCopyBuffer = fileName;
+        Debug.Log("Copy clipboard : \n" + fileName);
+    }
+}
